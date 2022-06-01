8dc8fc00dd72801c258e75bf3b9cd6003d3dff92
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-06-01 22:17:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  dodger.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if(e.key==="ArrowRight")
+    {
+        moveDodgerRight()
+    }
+  });
+
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const Right = parseInt(rightNumbers, 10);
+  
+    if (Right > 0) {
+      dodger.style.left = `${Right + 1}px`;
+    }
+  }
